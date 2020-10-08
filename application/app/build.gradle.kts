c5a7736bed7f7234426ab1dce4add4dc777358bd
Michael Emelyanov <emelyanovm85@gmail.com>
2020-10-08 18:01:38 +0300

    Added dagger ThemeComponent, ContextModule and base resources for app

--- application/app/build.gradle.kts
+++ application/app/build.gradle.kts
@@ -1,6 +1,12 @@
 androidBinary(
     packageName = "com.stepango.blockme",
     dependencies = deps(
+        androidx.appcompat,
+        androidx.core_ktx,
+        androidx.navigation_fragment_ktx,
+        google.material,
+        google.dagger
+    ) + deps(
         project(":root-library"),
         project(":common:extensions:android-util"),
         project(":core:mvvm:library"),
---
+++ application/app/src/main/java/com/stepango/blockme/app/App.kt
@@ -0,0 +1,10 @@
+package com.stepango.blockme.app
+
+import android.app.Application
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
