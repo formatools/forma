24ca3596323ced080bffdf164cdfab06255b7357
Stepan Goncharov <step.89.g@gmail.com>
2020-10-17 22:26:12 -0700

    Agp version update

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -4,7 +4,7 @@ androidProjectConfiguration(
     targetSdk = 29,
     compileSdk = 29,
     kotlinVersion = "1.4.10",
-    agpVersion = "4.1.0-rc03",
+    agpVersion = "4.1.0",
     versionCode = 1,
     versionName = "1.0",
     repositories = {
--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -11,7 +11,7 @@ version = "0.0.1"
 val kotlin_version = "1.4.10"
 
 dependencies {
-    implementation("com.android.tools.build:gradle:4.1.0-rc03")
+    implementation("com.android.tools.build:gradle:4.1.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
     implementation("org.funktionale:funktionale-either:1.2")
 }
