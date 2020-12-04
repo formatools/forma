e8c071ac80fd11a40cf1f58299332ebf2e87e929
Stepan Goncharov <step.89.g@gmail.com>
2020-12-03 23:16:27 -0800

    Minor refactoring

---
+++ plugins/android/src/main/java/tools/forma/android/target/AndroidTargets.kt
@@ -0,0 +1,15 @@
+package tools.forma.android.target
+
+object BinaryTarget : TargetSpec("binary")
+object ApplicationTarget : TargetSpec("app")
+object LibraryTarget : TargetSpec("library")
+object UtilTarget : TargetSpec("util")
+object TestUtilTarget : TargetSpec("test-util")
+object AndroidTestUtilTarget : TargetSpec("android-test-util")
+object AndroidUtilTarget : TargetSpec("android-util")
+object DataBindingTarget : TargetSpec("databinding")
+object DataBindingAdapterTarget : TargetSpec("databinding-adapter")
+object ResourcesTarget : TargetSpec("res")
+object ApiTarget : TargetSpec("api")
+object ImplTarget : TargetSpec("impl")
+object WidgetTarget : TargetSpec("widget")
