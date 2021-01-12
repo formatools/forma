1c9747a3548bfed52d383f84721d89be19b730f5
Alexander Krylov <a.krilov@cft.ru>
2021-01-12 08:18:54 +0700

    Replaced a direct using api keys to config. Used a clock interface instead the System.currentTimeMillis.

--- application/feature/characters/core/impl/src/main/java/com/stepango/blockme/feature/characters/core/impl/domain/repository/ServiceMarvelRepository.kt
+++ application/feature/characters/core/impl/src/main/java/com/stepango/blockme/feature/characters/core/impl/domain/repository/ServiceMarvelRepository.kt
@@ -17,63 +17,66 @@
 package com.stepango.blockme.feature.characters.core.impl.domain.repository
 
 import com.stepango.blockme.common.extensions.util.toMD5
+import com.stepango.blockme.common.util.clock.Clock
+import com.stepango.blockme.core.network.library.Config
 import com.stepango.blockme.core.network.library.response.BaseResponse
 import com.stepango.blockme.feature.characters.core.api.data.response.CharacterResponse
-import com.stepango.blockme.feature.characters.core.api.domain.repository.MarvelRepository
 import com.stepango.blockme.feature.characters.core.api.data.service.MarvelService
+import com.stepango.blockme.feature.characters.core.api.domain.repository.MarvelRepository
 import javax.inject.Inject
 
-// TODO https://github.com/formatools/forma/issues/45
-// Move out from here API keys into some config
-private const val API_PUBLIC_KEY = "34fc91a3d879f19895b515d8273965f9"
-private const val API_PRIVATE_KEY = "d66d90c45623d9a1daeb9a398a7ded8268a9628f"
 private const val HASH_FORMAT = "%s%s%s"
 
 /**
  * Repository module for handling marvel API network operations [MarvelService].
  */
 internal class ServiceMarvelRepository @Inject constructor(
-    private val service: MarvelService
+	private val service: MarvelService,
+	private val config: Config,
+	private val clock: Clock,
 ) : MarvelRepository {
 
-    /**
-     * Get all info of Marvel character.
-     *
-     * @param id A single character id.
-     * @return Response for single character resource.
-     */
-    override suspend fun getCharacter(id: Long): BaseResponse<CharacterResponse> {
-        val timestamp = System.currentTimeMillis().toString()
-        return service.getCharacter(
-            id = id,
-            apiKey = API_PUBLIC_KEY,
-            hash = generateApiHash(timestamp),
-            timestamp = timestamp)
-    }
+	/**
+	 * Get all info of Marvel character.
+	 *
+	 * @param id A single character id.
+	 * @return Response for single character resource.
+	 */
+	override suspend fun getCharacter(id: Long): BaseResponse<CharacterResponse> {
+		val timestamp = clock.currentTimeMillis().toString()
 
-    /**
-     * Get all Marvel characters by paged.
-     *
-     * @param offset Skip the specified number of resources in the result set.
-     * @param limit Limit the result set to the specified number of resources.
-     * @return Response for comic characters resource.
-     */
-    override suspend fun getCharacters(offset: Int, limit: Int): BaseResponse<CharacterResponse> {
-        val timestamp = System.currentTimeMillis().toString()
-        return service.getCharacters(
-            apiKey = API_PUBLIC_KEY,
-            hash = generateApiHash(timestamp),
-            timestamp = timestamp,
-            offset = offset,
-            limit = limit)
-    }
+		return service.getCharacter(
+			id = id,
+			apiKey = config.publicApiKey,
+			hash = generateApiHash(timestamp),
+			timestamp = timestamp
+		)
+	}
 
-    /**
-     * Generate a md5 digest of the timestamp parameter, private API key and public.
-     *
-     * @param timestamp A digital current record of the time.
-     * @return The MD5 Hash
-     */
-    private fun generateApiHash(timestamp: String) =
-        HASH_FORMAT.format(timestamp, API_PRIVATE_KEY, API_PUBLIC_KEY).toMD5()
+	/**
+	 * Get all Marvel characters by paged.
+	 *
+	 * @param offset Skip the specified number of resources in the result set.
+	 * @param limit Limit the result set to the specified number of resources.
+	 * @return Response for comic characters resource.
+	 */
+	override suspend fun getCharacters(offset: Int, limit: Int): BaseResponse<CharacterResponse> {
+		val timestamp = clock.currentTimeMillis().toString()
+		return service.getCharacters(
+			apiKey = config.publicApiKey,
+			hash = generateApiHash(timestamp),
+			timestamp = timestamp,
+			offset = offset,
+			limit = limit
+		)
+	}
+
+	/**
+	 * Generate a md5 digest of the timestamp parameter, private API key and public.
+	 *
+	 * @param timestamp A digital current record of the time.
+	 * @return The MD5 Hash
+	 */
+	private fun generateApiHash(timestamp: String) =
+		HASH_FORMAT.format(timestamp, config.privateApiKey, config.publicApiKey).toMD5()
 }
