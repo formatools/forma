f55b1ae1d85db6a4ee86f93d2d100478c2cdf9b0
Alexander Krylov <a.krilov@cft.ru>
2021-01-12 08:11:14 +0700

    Extend the config. Add api keys

--- application/core/network/library/src/main/java/com/stepango/blockme/core/network/library/Config.kt
+++ application/core/network/library/src/main/java/com/stepango/blockme/core/network/library/Config.kt
@@ -3,4 +3,6 @@ package com.stepango.blockme.core.network.library
 interface Config {
 
 	val baseUrl: String
+	val privateApiKey: String
+	val publicApiKey: String
 }
--- application/root-app/src/main/java/com/stepango/blockme/root/library/config/NetworkConfig.kt
+++ application/root-app/src/main/java/com/stepango/blockme/root/library/config/NetworkConfig.kt
@@ -1,7 +1,10 @@
 package com.stepango.blockme.root.library.config
+
 import com.stepango.blockme.core.network.library.Config
 
-class NetworkConfig: Config {
+class NetworkConfig : Config {
 
 	override val baseUrl: String = "https://gateway.marvel.com/"
+	override val privateApiKey: String = "d66d90c45623d9a1daeb9a398a7ded8268a9628f"
+	override val publicApiKey: String = "34fc91a3d879f19895b515d8273965f9"
 }
