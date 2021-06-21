17fa1d1979962bdf653d51e4e37005ed9e0faf86
Igor Karenkov <karenkovigor@gmail.com>
2021-06-21 11:25:32 +0300

    Add viewbinding and resources to impl

--- plugins/android/src/main/java/impl.kt
+++ plugins/android/src/main/java/impl.kt
@@ -1,9 +1,9 @@
-import tools.forma.android.utils.BuildConfiguration
-import tools.forma.android.validation.disallowResources
-import tools.forma.validation.validator
 import org.gradle.api.Project
 import tools.forma.android.feature.AndroidLibraryFeatureConfiguration
+import tools.forma.android.feature.androidLibraryFeatureDefinition
 import tools.forma.android.feature.applyFeatures
+import tools.forma.android.feature.kaptConfigurationFeature
+import tools.forma.android.feature.kotlinAndroidFeatureDefinition
 import tools.forma.android.target.AndroidUtilTargetTemplate
 import tools.forma.android.target.ApiTargetTemplate
 import tools.forma.android.target.DataBindingAdapterTargetTemplate
@@ -14,26 +14,24 @@ import tools.forma.android.target.ResourcesTargetTemplate
 import tools.forma.android.target.TestUtilTargetTemplate
 import tools.forma.android.target.UtilTargetTemplate
 import tools.forma.android.target.WidgetTargetTemplate
-import tools.forma.deps.applyDependencies
-import tools.forma.android.feature.androidLibraryFeatureDefinition
-import tools.forma.android.feature.kaptConfigurationFeature
-import tools.forma.android.feature.kotlinAndroidFeatureDefinition
+import tools.forma.android.utils.BuildConfiguration
 import tools.forma.deps.FormaDependency
 import tools.forma.deps.NamedDependency
+import tools.forma.deps.applyDependencies
 import tools.forma.validation.validate
+import tools.forma.validation.validator
 
 fun Project.impl(
     packageName: String,
     dependencies: FormaDependency = emptyDependency(),
     testDependencies: NamedDependency = emptyDependency(),
     androidTestDependencies: NamedDependency = emptyDependency(),
+    viewBinding: Boolean = true,
     testInstrumentationRunner: String = androidJunitRunner,
     buildConfiguration: BuildConfiguration = BuildConfiguration(),
     consumerMinificationFiles: Set<String> = emptySet(),
     manifestPlaceholders: Map<String, Any> = emptyMap()
 ) {
-
-    disallowResources()
 
     target.validate(ImplTargetTemplate)
     val libraryFeatureConfiguration = AndroidLibraryFeatureConfiguration(
@@ -42,7 +40,8 @@ fun Project.impl(
         testInstrumentationRunner,
         consumerMinificationFiles,
         manifestPlaceholders,
-        selfValidator = validator(ImplTargetTemplate)
+        selfValidator = validator(ImplTargetTemplate),
+        viewBinding = viewBinding
     )
     applyFeatures(
         androidLibraryFeatureDefinition(libraryFeatureConfiguration),
