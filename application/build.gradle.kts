ab6d34281de6eb896512aa16b51a0e817dbae001
Stepan Goncharov <sgoncharov@gradle.com>
2023-03-29 09:51:09 -0700

    Optimised project includes

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -1,5 +1,3 @@
-import java.util.Properties
-
 buildscript {
     androidProjectConfiguration(
         project = project,
--- application/settings.gradle.kts
+++ application/settings.gradle.kts
@@ -1,3 +1,17 @@
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+import kotlin.system.measureTimeMillis
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+        classpath("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.4")
+    }
+}
+
 dependencyResolutionManagement {
     @Suppress("UnstableApiUsage")
     repositories {
@@ -5,23 +19,52 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+val counter = java.util.concurrent.atomic.AtomicInteger(0)
 
 rootProject.name = "forma"
+measureTimeMillis {
+    runBlocking {
+        findGradleKtsFiles(rootDir)
+            .toList().forEach {
+                val moduleDir = it.parentFile
+                val relativePath = rootDir.toPath().relativize(moduleDir.toPath()).toString()
+                val moduleName = ":" + relativePath.replace(File.separator, "-")
 
-fileTree(".")
-    .filter {
-        it.isFile
-            && it.name.endsWith(".gradle.kts")
-            && !it.path.contains("buildSrc")
-            && !it.name.startsWith("settings.gradle") }
-    .forEach {
-        val moduleDir = it.parentFile
-        val relativePath = rootDir.toPath().relativize(moduleDir.toPath()).toString()
-        val moduleName = ":" + relativePath.replace(File.separator, "-")
+                include(moduleName)
 
-        include(moduleName)
+                val project = findProject(moduleName)!!
+                project.projectDir = moduleDir
+                project.buildFileName = it.name
 
-        val project = findProject(moduleName)!!
-        project.projectDir = moduleDir
-        project.buildFileName = it.name
+            }
     }
+}.let { println("Loaded in $it ms") }
+
+
+suspend fun findGradleKtsFiles(
+    root: File,
+    ignoredFilenames: List<String> = listOf("settings.gradle.kts"),
+    ignoredFolders: List<String> = listOf("build", "src", "buildSrc")
+): List<File> = coroutineScope {
+
+    val children = root.listFiles() ?: emptyArray()
+
+    val (dirs, files) = children.partition { it.isDirectory }
+
+    val gradleKtsFiles = if (root == rootDir) emptyList() else files
+        .filter { it.name.endsWith(".gradle.kts") }
+        .filterNot { it.isHidden || it.name in ignoredFilenames }
+
+    val dirJobs = dirs
+        .filterNot { it.isHidden || it.name in ignoredFolders }
+        .map { dir ->
+            async(Dispatchers.IO) {
+                findGradleKtsFiles(dir, ignoredFilenames, ignoredFolders)
+            }
+        }
+
+    val nestedGradleKtsFiles = dirJobs.awaitAll().flatten()
+
+    gradleKtsFiles + nestedGradleKtsFiles
+}
+
