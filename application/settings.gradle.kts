68edbd3d65e6d5a27f352dee120f1908bb5eff54
Stepan Goncharov <sgoncharov@gradle.com>
2023-04-13 12:21:12 -0700

    Includer v0.1.1

--- application/settings.gradle.kts
+++ application/settings.gradle.kts
@@ -1,15 +1,5 @@
-import kotlinx.coroutines.*
-import kotlin.system.measureTimeMillis
-
-buildscript {
-    repositories {
-        mavenCentral()
-        google()
-    }
-    dependencies {
-        classpath("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-        classpath("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.4")
-    }
+plugins {
+    id("tools.forma.includer") version "0.1.1"
 }
 
 dependencyResolutionManagement {
@@ -20,52 +10,4 @@ dependencyResolutionManagement {
     }
 }
 
-val counter = java.util.concurrent.atomic.AtomicInteger(0)
-
 rootProject.name = "application"
-measureTimeMillis {
-    runBlocking {
-        findGradleKtsFiles(rootDir)
-            .toList().forEach {
-                val moduleDir = it.parentFile
-                val relativePath = rootDir.toPath().relativize(moduleDir.toPath()).toString()
-                val moduleName = ":" + relativePath.replace(File.separator, "-")
-
-                include(moduleName)
-
-                val project = findProject(moduleName)!!
-                project.projectDir = moduleDir
-                project.buildFileName = it.name
-
-            }
-    }
-}.let { println("Loaded in $it ms") }
-
-
-suspend fun findGradleKtsFiles(
-    root: File,
-    ignoredFilenames: List<String> = listOf("settings.gradle.kts"),
-    ignoredFolders: List<String> = listOf("build", "src", "buildSrc")
-): List<File> = coroutineScope {
-
-    val children = root.listFiles() ?: emptyArray()
-
-    val (dirs, files) = children.partition { it.isDirectory }
-
-    val gradleKtsFiles = if (root == rootDir) emptyList() else files
-        .filter { it.name.endsWith(".gradle.kts") }
-        .filterNot { it.isHidden || it.name in ignoredFilenames }
-
-    val dirJobs = dirs
-        .filterNot { it.isHidden || it.name in ignoredFolders }
-        .map { dir ->
-            async(Dispatchers.IO) {
-                findGradleKtsFiles(dir, ignoredFilenames, ignoredFolders)
-            }
-        }
-
-    val nestedGradleKtsFiles = dirJobs.awaitAll().flatten()
-
-    gradleKtsFiles + nestedGradleKtsFiles
-}
-
---
+++ includer/Readme.md
@@ -0,0 +1,13 @@
+# Includer
+
+Simple and powerful plugin which helps you to save time and avoid writing tons of boilerplate code. Uses kotlin coroutines to efficiently traverse even deeply nested project trees.
+
+## Installation
+
+Remove all your `include` declarations from your `settings.gradle.kts` file and replace them with:
+
+```kotlin
+plugins {
+    id("tools.forma.includer") version "0.1.1"
+}
+```
