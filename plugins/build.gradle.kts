83335c88da737d7ceb17563fcc8f456d6d1b3f2c
Stepan Goncharov <sgoncharov@gradle.com>
2023-07-09 06:46:46 -0700

    Forma Android v0.1.0

--- plugins/build.gradle.kts
+++ plugins/build.gradle.kts
@@ -1,47 +1,9 @@
-import com.gradle.publish.PublishPlugin
-
 plugins {
     `kotlin-dsl`
-    id("com.gradle.plugin-publish") version "1.1.0" apply false
+    id("com.gradle.plugin-publish") version "1.2.0" apply false
 }
 
 class FormaRootConfigurationException(
     override val message: String,
     override val cause: Throwable? = null
 ) : Exception()
-
-subprojects {
-    plugins.whenPluginAdded {
-        when (this) {
-            is PublishPlugin -> registerPublishingTasks()
-        }
-    }
-}
-
-fun Project.registerPublishingTasks() {
-    /**
-     * Workaround from https://github.com/gradle/gradle/issues/1246
-     */
-    val pluginPublishKeysSetup = tasks.register("pluginPublishKeysSetup") {
-        doLast {
-            val key = System.getenv("GRADLE_PUBLISH_KEY")
-            val secret = System.getenv("GRADLE_PUBLISH_SECRET")
-
-            if (key == null || secret == null) throw GradleException(
-                "gradlePublishKey and/or gradlePublishSecret are not defined environment variables"
-            )
-
-            System.setProperty("gradle.publish.key", key)
-            System.setProperty("gradle.publish.secret", secret)
-        }
-    }
-
-    tasks.named("publishPlugins").configure {
-        dependsOn(pluginPublishKeysSetup)
-    }
-}
-
-
-tasks.register("publishPluginsToMavenLocal") {
-    dependsOn(subprojects.map { "${it.path}:publishToMavenLocal" })
-}
